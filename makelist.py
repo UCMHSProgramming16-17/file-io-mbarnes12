129f980d235f5a40f47dbbea7562cfded079dae2
Malina Barnes <mbarnes@ucvts.org>
2016-12-12 18:20:52 +0000

    added list program

---
+++ makelist.py
@@ -0,0 +1,11 @@
+file = open('listfile.txt', 'w')
+
+file.write(input('SPEAK! ') + '\n')
+
+file.write('Hello Anqi')
+file.write('Hi Thomas')
+file.write('Hello David')
+file.write('Hi Sam')
+file.write('Hi Matt')
+
+file.close()
